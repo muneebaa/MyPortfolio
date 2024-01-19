cdaad5b23fc3edd8084bcca3a380dcd44804d4b2
Muneeba Dilawaze <muneeba.dilawaze@gmail.com>
2024-01-19 17:53:05 +0500

    pushcode

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import { AboutMe, Contact, Project, Skill, Home } from "./mainComponents";
 import Navbar from "./commonComponents/Navbar";
 import MyExperience from "./mainComponents/MyExperience";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <ToastContainer />
       <Navbar />
       <Home />
       <Skill />
--- src/mainComponents/Contact/Contact.jsx
+++ src/mainComponents/Contact/Contact.jsx
@@ -1,8 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Footer from "../../commonComponents/Footer/Footer";
+import emailjs from "@emailjs/browser";
+import {
+  Audio,
+  Discuss,
+  FidgetSpinner,
+  LineWave,
+  MutatingDots,
+  Puff,
+} from "react-loader-spinner";
+import { toast } from "react-toastify";
 import "./style.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
+  const form = useRef();
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState({
+    from_name: "",
+    from_email: "",
+    message: "",
+  });
+  const notifySuccess = () => toast.success("Wohoo! Email sent successfully.");
+  const notifyError = (err) => toast.error(err);
+
+  const sendEmail = () => {
+    setLoading(true);
+
+    if (data.from_email && data.from_name && data.message) {
+      console.log("asdas--", form.current);
+      emailjs
+        .sendForm(
+          "service_1omkr04",
+          "template_1qi43qc",
+          form.current,
+          "RGHtqO4VBcytDSOen"
+        )
+        .then(
+          (result) => {
+            setLoading(false);
+            notifySuccess();
+            console.log(result.text);
+          },
+          (error) => {
+            setLoading(false);
+            notifyError("Oops! Something went wrong.");
+            console.log(error.text);
+          }
+        );
+    } else {
+      setLoading(false);
+      notifyError("Fill all fields to send email");
+    }
+  };
+  useEffect(() => {
+    console.log("data", data);
+  }, [data]);
   return (
     <div>
       <div className="contact_main spacing_main">
@@ -10,12 +63,74 @@ const Contact = () => {
           Get in <span className="text_bold">Touch with me</span>
         </h1>
         <div className="contact_main_wrapper">
-          <div className="contact_section1">
+          {/* <form ref={form} className="contact_section1">
             <input type="text" placeholder="Your name" />
             <input type="text" placeholder="Email" />
             <textarea type="text" placeholder="How can I help?*" />
-            <button>Get In Touch</button>
-          </div>
+            <button onClick={(e) => sendEmail(e)}>Get In Touch</button>
+          </form> */}
+
+          <form ref={form} className="contact_section1">
+            <label>Name</label>
+            <input
+              type="text"
+              name="from_name"
+              placeholder="John Doe"
+              onChange={(e) =>
+                setData((prevState) => ({
+                  ...prevState,
+                  from_name: e.target.value,
+                }))
+              }
+            />
+            <label>Email</label>
+            <input
+              type="email"
+              name="from_email"
+              onChange={(e) =>
+                setData((prevState) => ({
+                  ...prevState,
+                  from_email: e.target.value,
+                }))
+              }
+              placeholder="john.doe@gmail.com"
+            />
+            <label>Message</label>
+            <textarea
+              name="message"
+              placeholder="How can I help?*"
+              onChange={(e) =>
+                setData((prevState) => ({
+                  ...prevState,
+                  message: e.target.value,
+                }))
+              }
+            />
+            <div
+              onClick={() => sendEmail()}
+              className="contact_submit_btn"
+              style={{
+                pointerEvents: loading ? "none" : null,
+                backgroundColor: loading ? "#cdcdcd" : "black",
+                color: loading ? "#5A5A5A" : "white",
+              }}
+            >
+              <span> Get In Touch</span>
+              <Puff
+                visible={loading}
+                height="20"
+                width="20"
+                color="#5A5A5A"
+                ariaLabel="puff-loading"
+                wrapperStyle={{
+                  width: "20px",
+                  height: "20px",
+                  marginLeft: "10px",
+                }}
+                wrapperClass=""
+              />
+            </div>
+          </form>
           <div className="contact_section2">
             <h1>
               Lets <span className="text_outline">talk</span> for
