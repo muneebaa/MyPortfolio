8535a3d39ca6bf0e29049d16c917875749e77e80
Muneeba Dilawaze <muneeba.dilawaze@gmail.com>
2024-03-30 12:37:37 +0500

    experience added

--- src/mainComponents/MyExperience/MyExperience.jsx
+++ src/mainComponents/MyExperience/MyExperience.jsx
@@ -19,7 +19,22 @@ const MyExperience = () => {
             image={zweidevsLogo}
             title={"Zweidevs"}
             description={
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiamolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquamnihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,quia."
+              <div>
+                <p>
+                  As a Software Engineer at Zweidevs, I successfully completed
+                  over <b>10 projects</b> spanning web and mobile applications.
+                  During this tenure, I acquired expertise in technologies such
+                  as
+                  <b> React, React Native, Firebase, Craft.js, Next.js,</b> and
+                  various other tools, contributing significantly to my
+                  professional development.
+                </p>
+                <p>
+                  Additionally, I had the opportunity to collaborate with
+                  <b> clients</b> from <b>around the world</b>, ensuring
+                  exceptional experiences and outcomes for their projects
+                </p>
+              </div>
             }
             start_date={"Dec 2022"}
             end_date={"Present"}
@@ -29,9 +44,23 @@ const MyExperience = () => {
             image={essLogo}
             title={"Expert System Solution"}
             description={
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiamolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquamnihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,quia."
+              <div>
+                As an Associate Software Engineer at Expert System Solutions, I
+                have successfully contributed to multiple projects utilizing
+                React.js, where I gained a deep understanding of core concepts
+                such as Redux and API integrations. My proactive approach and
+                problem-solving skills have significantly contributed to
+                streamlining project workflows, resulting in quicker and more
+                efficient project deliveries for the company.
+                <p>
+                  I collaborated with a diverse team, learning about version
+                  control systems and agile development practices. These
+                  experiences enhanced my technical skills and prepared me for
+                  future roles in software engineering.
+                </p>
+              </div>
             }
-            start_date={"Mar 2022"}
+            start_date={"May 2022"}
             end_date={"Dec 2022"}
             link={"https://esspk.com/"}
           />
@@ -40,10 +69,17 @@ const MyExperience = () => {
             image={vuLogo}
             title={"VU360 Solutions"}
             description={
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiamolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentiumoptio, eaque rerum! Provident similique accusantium nemo autem. Veritatisobcaecati tenetur iure eius earum ut molestias architecto voluptate aliquamnihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,quia."
+              <p>
+                As an intern at Vu360 Solutions, I gained valuable hands-on
+                experience working with React.js while actively contributing to
+                a CRM project. This experience not only enhanced my technical
+                skills but also provided me with insights into real-world
+                software development processes, preparing me for future roles in
+                the industry.
+              </p>
             }
             start_date={"Apr 2022"}
-            end_date={"Mar 2022"}
+            end_date={"May 2022"}
             link={"https://vu360solutions.com/"}
           />
         </div>
